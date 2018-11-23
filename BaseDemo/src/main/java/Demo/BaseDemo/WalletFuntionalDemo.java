6bd618b9c4ee494d774ccfd1cb0bebee0c4f7629
DavidJones <davidgluttongk@outlook.com>
2018-11-23 08:38:04 -0800

    Base Function Demo
    Add some basic funcations demo

--- BaseDemo/src/main/java/Demo/BaseDemo/WalletFuntionalDemo.java
+++ BaseDemo/src/main/java/Demo/BaseDemo/WalletFuntionalDemo.java
@@ -1,10 +1,22 @@
 package Demo.BaseDemo;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+
 public class WalletFuntionalDemo {
 
-
+    private static final Logger log = LoggerFactory.getLogger(WalletFuntionalDemo.class);
 
     public static void main(String[] args){
+        new WalletFuntionalDemo().run();
+
+    }
+
+    public void run(){
+
 
     }
 }
