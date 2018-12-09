02bf07989a0f5d35144444ed963ac5f008c7822f
DavidJones <davidgluttongk@outlook.com>
2018-12-09 11:31:25 -0800

    Clean up

--- BaseDemo/src/main/java/Demo/ContractDemo/GreetContract/GreeterContract.java
+++ BaseDemo/src/main/java/Demo/ContractDemo/GreetContract/GreeterContract.java
@@ -9,12 +9,10 @@ import org.chain3j.crypto.Credentials;
 import org.chain3j.protocol.Chain3j;
 import org.chain3j.protocol.core.RemoteCall;
 import org.chain3j.protocol.core.methods.response.*;
-import org.chain3j.protocol.exceptions.TransactionException;
 import org.chain3j.tx.Contract;
 import org.chain3j.tx.exceptions.ContractCallException;
 import org.chain3j.tx.gas.ContractGasProvider;
 
-import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Arrays;
 import java.util.Collections;
@@ -28,18 +26,12 @@ public class GreeterContract extends Contract {
         super(contractBinary,contractAddress,chain3j, credentials,provider);
     }
 
-//    private GreeterContract(String contractAddress, Chain3j chain3j, Credentials credentials, BigInteger gasPrice, BigInteger gasLimit) {
-//        super(contractAddress, chain3j, credentials, gasPrice, gasLimit);
-//    }
-
 
 
     public void init(){
-        //super.init();
-        System.out.println("this is greetContractClass");
+        //
     }
 
-    //public Future<Greeter> deployMe(
     public static  GreeterContract deployMe(
             Chain3j chain3j, Credentials credentials,
             ContractGasProvider provider, String contractBinary, Utf8String _greeting,
@@ -50,12 +42,10 @@ public class GreeterContract extends Contract {
                         Arrays.<Type>asList(_greeting)
                 );
         try{
-            //Future<Greeter> future =
             return deploy(GreeterContract.class,chain3j,credentials,provider,
                     contractBinary,encodedConstrutor,initialValue); //.get();
             //return greeter;
         }catch(ContractCallException e1){
-            System.out.println("greet contract exception:"+e1);
             return null;
         }
         catch(Exception e){
@@ -64,24 +54,15 @@ public class GreeterContract extends Contract {
         }
     }
 
-    //public RemoteCall
-
-   // public Future<Utf8String> greet() {
         public RemoteCall<Utf8String> greet(){
-        //Function function = new Function("greet",bi
-        //Function function = new Function("getGreeting",
         Function function = new Function("greet",
                 Arrays.<Type>asList(), Arrays.<TypeReference<?>>asList(
                 new TypeReference<Utf8String>() {
                 }));
         try
         {
-            //return executeCallSingleValueReturn(function);  //,String.class);
             return executeRemoteCallSingleValueReturn(function);
-        } /*catch (ContractCallException e1) {
-            System.out.println("greet contract exception:" + e1);
-            return null;
-        } */catch (Exception e) {
+        }catch (Exception e) {
             System.out.println("basedemo greet exception:" + e);
             return null;
         }
@@ -91,32 +72,11 @@ public class GreeterContract extends Contract {
     public RemoteCall<TransactionReceipt> newGreeting(Utf8String _greeting){
         Function function = new Function( "newGreeting",
                 Arrays.<Type>asList(_greeting),
-                //Arrays.<TypeReference<?>>asList(new TypeReference<Utf8String>(){})
                 Collections.<TypeReference<?>>emptyList()
                 );
         return executeRemoteCallTransaction(function);
-        //return exe(function);
     }
 
     //without sign
-
-//    public Future<TransactionReceipt> newGreeting(Utf8String _greeting) throws Exception{
-//        Function function = new Function("newGreeting", Arrays.<Type>asList(_greeting), Collections.<TypeReference<?>>emptyList());
-//        return (Future<TransactionReceipt>) executeTransaction(function);
-//    }
-
-
-
-//    public TransactionReceipt newGreeting(String _greeting) throws IOException, TransactionException {
-//        Function function = new Function( "newGreeting",
-//                Arrays.<Type>asList(new Utf8String(_greeting)),
-//                //Arrays.<TypeReference<?>>asList(new TypeReference<Utf8String>(){})
-//                Collections.<TypeReference<?>>emptyList()
-//        );
-//        //return executeTransaction(function);
-//        return executeCallSingleValueReturn(function);
-//    }
-
-
 
 }
--- BaseDemo/src/main/java/Demo/ContractDemo/GreetContract/MyContract.java
+++ BaseDemo/src/main/java/Demo/ContractDemo/GreetContract/MyContract.java
@@ -48,75 +48,6 @@ public class MyContract {
         new MyContract().run();
     }
 
-    public class Greeter extends Contract{
-        /* private Greeter(
-                 String contractAddress,Chain3j chain3j,Credentials credentials,
-                 BigInteger gasPrice,BigInteger gasLimit
-         ){
-             //String constractBinary = "";
-             //super(contractAddress,chain3j,credentials,gasPrice,gasLimit);
-            // DefaultGasProvider provider = new DefaultGasProvider();
-            // super(constractBinary,contractAddress,chain3j, credentials,provider);
-         }*/
-        private Greeter(
-                String  contractBinary,
-                String contractAddress,Chain3j chain3j,Credentials credentials,
-                ContractGasProvider provider
-        ){
-            //String constractBinary = "";
-            //super(contractAddress,chain3j,credentials,Contract.GAS_PRICE,Contract.GAS_LIMIT);//gasPrice,gasLimit);
-            // DefaultGasProvider provider = new DefaultGasProvider();
-            super(contractBinary,contractAddress,chain3j, credentials,provider);
-        }
-
-        //public Future<Greeter> deployMe(
-        public Greeter deployMe(
-                Chain3j chain3j,Credentials credentials,
-                ContractGasProvider provider,String contractBinary,Utf8String _greeting,
-                BigInteger initialValue
-        ){
-            String encodedConstrutor=
-                    FunctionEncoder.encodeConstructor(
-                            Arrays.<Type>asList(_greeting)
-                    );
-            try{
-                //Future<Greeter> future =
-                return deploy(Greeter.class,chain3j,credentials,provider,
-                        contractBinary,encodedConstrutor,initialValue); //.get();
-                //return greeter;
-            }catch(ContractCallException e1){
-                System.out.println("greet contract exception:"+e1);
-                return null;
-            }
-            catch(Exception e){
-                System.out.println("deploy exception:"+e);
-                return null;
-            }
-        }
-
-        //public Future<Utf8String> greet() {
-            public RemoteCall<Utf8String> greet(){
-            //Function function = new Function("greet",
-            //Function function = new Function("getGreeting",
-            Function function = new Function("getGreeting",
-                    Arrays.<Type>asList(), Arrays.<TypeReference<?>>asList(
-                    new TypeReference<Utf8String>() {
-                    }));
-            try
-            {
-                //return executeCallSingleValueReturn(function);  //,String.class);
-                return executeRemoteCallSingleValueReturn(function);
-            }catch(ContractCallException e1){
-                System.out.println("greet contract exception:"+e1);
-                return null;
-            }catch (Exception e){
-                System.out.println("basedemo greet exception:"+e);
-                return null;
-            }
-
-        }
-    }
-
     private void run() throws Exception {
 
 
@@ -124,97 +55,29 @@ public class MyContract {
                 "http://127.0.0.1:8545"));  // Use local MOAC server;
 //        Admin chain3j = Admin.build(new HttpService("http://127.0.0.1:8545"));
 
-//        System.out.println("Out Connected to MOAC client version: "
-//                + chain3j.chain3ClientVersion().send().getChain3ClientVersion());
-//
-//        System.out.println("block number:"
-//                + chain3j.mcBlockNumber().send().getBlockNumber());
-
-//        System.out.println("net peer Count:"
-//                + chain3j.netPeerCount().send().getQuantity());
-
-        //Load the wallet info from a keystore file
         Credentials credentials = LoadCredentialsFromKeystoreFile("test123");   //test123");
-//        String src = credentials.getAddress();
-//        System.out.println("Load address: " + src);
 
-
-        //BigInteger gasPrice = chain3j.mcGasPrice().send().getGasPrice();
         BigInteger gasLimit = BigInteger.valueOf(80_000);
-        //DefaultGasProvider provider = new DefaultGasProvider();
         String contractBinary = "123";
         String contractAddress =
                 "0x316C14F71334fB3D873fF0F797825C65277B64D5";
                 //"0x316C14F71334fB3D873fF0F797825C65277B64D5"; //"greet" smart cotract on private chain
 
-        //GreeterContract greeter = new GreeterContract(contractBinary,contractAddress,chain3j,credentials,provider);
-        //String chainId = chain3j.netVersion().send().getNetVersion();
         StaticGasProvider gasProvider = new StaticGasProvider(BigInteger.valueOf(80_000_000),gasLimit);
         GreeterContract greeterContract = new GreeterContract(contractBinary,contractAddress,chain3j,credentials,gasProvider);
-        //Greeter loadContract = Greeter.load();
-        //GreeterContract greeter = GreeterContract.deployMe(chain3j, credentials,
-        //        provider, contractBinary, new Utf8String("Hello world"),
-        //        BigInteger.ZERO );
-        System.out.println("new Contract");
-        //System.out.println(greeterContract.isValid());
+
+
         if( greeterContract==null ){//isValid())){
             System.out.println(("greeter is null"));
         }
 
-        //greeterContract.newGreeting("hi,change it").sendAsync();
         greeterContract.newGreeting(new Utf8String("hi,testing 2019")).send();
-        //greeterContract.newGreeting("hi,change it");
-        //TransactionReceipt tReceipt = greeterContract.newGreeting("123321").get()
-        System.out.println("have using newGreetng");
 
-//        //Future<Utf8String> valueFuture =
         try{
             System.out.println("test123321 "+greeterContract.greet().send());
         }catch (Exception e){
             e.printStackTrace();
         }
-//        try{
-//            Utf8String test2 = greeterContract.greet().send();
-//            if(test2==null){
-//                System.out.println("test 2 is null");
-//            }else{
-//                System.out.println("test123321 "+test2.getValue());
-//            }
-//
-//        }catch (Exception e){
-//            e.printStackTrace();
-//        }
-
-
-
-        //Utf8String test2 = greeterContract.greet().send();
-        //String valueString = test2.getTypeAsString();
-       //System.out.println("test1121 = "+valueString);
-//        if(valueFuture == null){
-//            System.out.println("future is null");
-//        }
-        //Utf8String valueUtf8 = valueFuture.get();
-        //String valueString = valueUtf8.getTypeAsString();
-
-
-//        System.out.println("geeter greet:"+
-//                greeter.greet().get().getTypeAsString());
-        //System.out.println("greeter greet:"+
-        //greeter.greet().send());
-
-//        // Build the Raw TX
-//        BigInteger sendValue = BigInteger.valueOf(1000000000000L);
-//        String des = "0x7312F4B8A4457a36827f185325Fd6B66a3f8BB8B";
-//        RawTransaction rawTx  = createTX(srcNonce, des, sendValue);
-//
-//        // Sign the TX with Credential
-//        byte[] signedTX = TransactionEncoder.signTxEIP155(rawTx, 100, credentials);
-//        String signedRawTx = Numeric.toHexString(signedTX);
-//
-//        System.out.println("Signed RawTX: "+signedRawTx);
-//
-//        // Send the TX to the network and wait for the results
-//        System.out.println("MOAC TX send: " + chain3j.mcSendRawTransaction(signedRawTx).send());
 
     }
 
